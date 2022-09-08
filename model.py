5e9d687510d0ebf07c6c9a6275eb9592edb4bc28
praneet9 <praneetbomma@gmail.com>
2022-09-08 22:51:56 +0530

    model w/o global pool

--- model.py
+++ model.py
@@ -23,3 +23,34 @@ class ModelGP(nn.Module):
         x = torch.add(image, embs)
         
         return self.output(x)
+
+
+class ModelRaw(nn.Module):
+    
+    def __init__(self, output_vocab, seq_len=24):
+        
+        super().__init__()
+        
+        self.lstm_1 = nn.LSTM(300, 96, batch_first=True, num_layers=2, bidirectional=True, dropout=0.5)
+        self.flatten = nn.Flatten()
+        self.fc_1 = nn.Linear(seq_len*96*2, 768)
+        
+        self.lstm_2 = nn.LSTM(768, seq_len, batch_first=True, num_layers=2, bidirectional=True, dropout=0.5)
+        
+        self.output = nn.Sequential(nn.Flatten(),
+                                    nn.Linear(197*seq_len*2, 768),
+                                    nn.ReLU(),
+                                    nn.Linear(768, 384),
+                                    nn.ReLU(),
+                                    nn.Linear(384, output_vocab))
+        
+    def forward(self, image, embs):
+        
+        embs, _ = self.lstm_1(embs)
+        embs = self.flatten(embs)
+        embs = self.fc_1(embs).unsqueeze(1)
+        
+        fused = torch.mul(image, embs)
+        fused, _ = self.lstm_2(fused)
+        
+        return self.output(fused)
